1866f23407f66a6f4f512f797960eb5798bb9ef5
Aaron Toponce <aaron.toponce@gmail.com>
2013-12-31 22:04:35 -0700

    create more of the initial skeleton

--- index.py
+++ index.py
@@ -1,49 +1,57 @@
 import os
 import base64
-from Crypto.Cipher import AES
+from Crypto.Cipher import Blowfish
 from Crypto import Random
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-key = Random.new().read(16)
+key = Random.new().read(8)
 
-def encrypt(raw):
-    pad = lambda s: s + (16 - len(s) % 16) * chr(16 - len(s) % 16)
-    plaintext = pad(raw)
-    iv = Random.new().read(16)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return(iv + cipher.encrypt(raw)).encode("hex")
+def encrypt(plaintext, new_url):
+    pad = lambda s: s + (8 - len(s) % 8) * chr(8 - len(s) % 8)
+    f = open('data/%s' % new_url, w)
+    plaintext = pad(plaintext)
+    iv = Random.new().read(8)
+    cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv)
+    ciphertext = iv + cipher.encrypt(plaintext).encode("hex")
+    f.write(ciphertext)
+    f.close()
+    return(ciphertext)
 
-def decrypt(raw):
+def decrypt(ciphertext):
     unpad = lambda s : s[0:-ord(s[-1])]
-    iv = raw[:16]
-    raw = raw[16:]
-    ciphertext = raw.decode("hex")
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return unpad(cipher.decrypt(raw))
-
-def create_file(u):
-    encrypt(u)
+    f = open('data/%s' % ciphertext, r)
+    iv = f[:8]
+    message = f[8:]
+    message = message.decode("hex")
+    cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv)
+    close(f)
+    return unpad(cipher.decrypt(message))
 
 def create_url():
-    u = base64.urlsafe_b64encode(os.urandom(16))
-    make_dir(u)
-    return u
-
-@app.route('/', methods=['POST'])
+    new_url = base64.urlsafe_b64encode(os.urandom(16))
+    if os.path.exists('data/%s' % new_url):
+        create_url()
+    
+@app.route('/', methods = ['POST','GET'])
 def index():
-    error = None
+    new_url = create_url()
     if request.method == 'POST':
-        u = create_url()
-        p = request.form['paste']
-    else:
-        error = 'Nothing submitted.'
-    return render_template('index.html', error=error)
+        plaintext = request.form['paste']
+        ciphertext = encrypt(plaintext, new_url)
+        show_post(new_url)
+    return render_template('index.html', random = new_url)
+
+@app.route('/post/<new_url>', methods = ['POST', 'GET'])
+def show_post(new_url):
+    return render_template('post.html', random = new_url)
 
 @app.route('/<random_url>')
 def fetch_url(random_url):
-    pass
+    plaintext = decrypt(random_url)
+    return render_template('note.html', text = plaintext)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    app.run()
