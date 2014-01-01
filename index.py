9000880544d6db934d7e12780ef654ca450f2bb6
Aaron Toponce <aaron.toponce@gmail.com>
2014-01-01 10:12:51 -0700

    switch blowfish to ebc mode (meh). form data not getting caught. wtf

--- index.py
+++ index.py
@@ -6,28 +6,27 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Change me.
+# Change me. Should be at least 16 bytes long.
+# strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 16 | tr -d '\n'; echo
 key = "cN7RPiuMhJwX1e9MUwuTXggpK9r2ym"
 
-def encrypt(key, plaintext, new_url):
+def note_encrypt(key, plaintext, new_url):
     pad = lambda s: s + (8 - len(s) % 8) * chr(8 - len(s) % 8)
     f = open('data/%s' % new_url, 'w')
-    plaintext = pad(f)
-    iv = Random.new().read(8)
-    cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv)
-    ciphertext = iv + cipher.encrypt(plaintext).encode("base64")
+    plaintext = pad(plaintext)
+    cipher = Blowfish.new(key, Blowfish.MODE_ECB)
+    ciphertext = cipher.encrypt(plaintext).encode("base64")
     f.write(ciphertext)
     f.close()
     return(ciphertext)
 
-def decrypt(ciphertext):
+def note_decrypt(ciphertext):
     unpad = lambda s : s[0:-ord(s[-1])]
     f = open('data/%s' % ciphertext, 'r')
-    iv = f[:8]
-    message = f[8:]
-    close(f)
+    message = f.read()
+    f.close()
     message = message.decode("base64")
-    cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv)
+    cipher = Blowfish.new(key, Blowfish.MODE_ECB)
     return unpad(cipher.decrypt(message))
 
 def create_url():
@@ -41,10 +40,8 @@ def index():
     new_url = create_url()
     error = None
     if request.method == 'POST':
-        if request.form['pass']:
-            key = request.form['pass']
         plaintext = request.form['paste']
-        ciphertext = encrypt(key, plaintext, new_url)
+        ciphertext = note_encrypt(key, plaintext, new_url)
         show_post(new_url)
     else:
         error = "Invalid data."
@@ -56,7 +53,7 @@ def show_post(new_url):
 
 @app.route('/<random_url>')
 def fetch_url(random_url):
-    plaintext = decrypt(random_url)
+    plaintext = note_decrypt(random_url)
     return render_template('note.html', text = plaintext)
 
 if __name__ == '__main__':
