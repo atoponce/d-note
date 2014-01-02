018204bff62a67d3745eac5d763ded2981bfee42
Aaron Toponce <atoponce@xmission.com>
2014-01-02 12:16:19 -0700

    add async decorator for future code to delete old notes

--- d-note.py
+++ d-note.py
@@ -1,64 +1,77 @@
+import base64
 import os
 import zlib
-import base64
 from Crypto import Random
 from Crypto.Cipher import Blowfish
 from flask import Flask, render_template, request
+from threading import Thread
 
-app = Flask(__name__)
+dnote = Flask(__name__)
 
-# Change me. Should be at least 16 bytes long.
+# CHANGEME. Should be at least 16 bytes long.
 # strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 16 | tr -d 'n'; echo
 key = "cN7RPiuMhJwX1e9MUwuTXggpK9r2ym"
 
+def async(func):
+    def wrap(*args, **kwargs):
+        t = Thread(target = func, args = args, kwargs = kwargs)
+        t.start()
+    return wrap
+
+@async
+def note_destroy()
+    while True:
+        # code to identify which files to destroy
+
 def note_encrypt(key, plaintext, new_url):
-    app.logger.debug('note_encrypt')
+    dnote.logger.debug('note_encrypt')
     pad = lambda s: s + (8 - len(s) % 8) * chr(8 - len(s) % 8)
     plain = pad(zlib.compress(plaintext))
     with open('data/%s' % new_url, 'w') as f:
-        cipher = Blowfish.new(key, Blowfish.MODE_ECB)
-        ciphertext = cipher.encrypt(plain)
+        bf = Blowfish.new(key, Blowfish.MODE_ECB)
+        ciphertext = bf.encrypt(plain)
         f.write(ciphertext.encode("base64"))
 
 def note_decrypt(ciphertext):
-    app.logger.debug('note_decrypt')
+    dnote.logger.debug('note_decrypt')
     unpad = lambda s : s[0:-ord(s[-1])]
     with open('data/%s' % ciphertext, 'r') as f:
         message = f.read()
-    cipher = Blowfish.new(key, Blowfish.MODE_ECB)
-    plaintext = cipher.decrypt(message.decode("base64"))
+    bf = Blowfish.new(key, Blowfish.MODE_ECB)
+    plaintext = bf.decrypt(message.decode("base64"))
     return zlib.decompress(unpad(plaintext))
 
 def create_url():
-    app.logger.debug('create_url')
+    dnote.logger.debug('create_url')
     new_url = base64.urlsafe_b64encode(Random.new().read(16))[:22]
     if os.path.exists('data/%s' % new_url):
         create_url()
     return new_url
     
-@app.route('/', methods = ['POST','GET'])
+@dnote.route('/', methods = ['POST','GET'])
 def index():
-    app.logger.debug('default route')
+    dnote.logger.debug('default route')
     error = None
     new_url = create_url()
     return render_template('index.html', random = new_url, error=error)
 
-@app.route('/post/<new_url>', methods = ['POST', 'GET'])
+@dnote.route('/post/<new_url>', methods = ['POST', 'GET'])
 def show_post(new_url):
     if request.method == 'POST':
-        app.logger.debug('handle post request')
+        dnote.logger.debug('handle post request')
         plaintext = request.form['paste']
-        app.logger.debug('create cipher file')
+        dnote.logger.debug('create cipher file')
         note_encrypt(key, plaintext, new_url)
     else:
         error = "Invalid data."
     return render_template('post.html', random = new_url)
 
-@app.route('/<random_url>')
+@dnote.route('/<random_url>')
 def fetch_url(random_url):
     plaintext = note_decrypt(random_url)
     return render_template('note.html', text = plaintext)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    dnote.debug = True
+    note_destroy()
+    dnote.run()
