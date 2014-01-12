8dc46aa9e3d04a110e25d78e6b4251353f9e61ba
Aaron Toponce <aaron.toponce@gmail.com>
2014-01-12 08:42:28 -0700

    working towards a token minting notification on submission. not working

--- static/js/main.js
+++ static/js/main.js
@@ -7,12 +7,19 @@ function make_key() {
     return text;
 }
 
+function please_wait() {
+    span = document.getElementById('progress');
+    span.style.visibility = 'visible';
+}
+
 function validate_form() {
     if(document.getElementById('paste').value == "") {
         alert("You need to enter a message.");
         return false;
     }
     else {
+        please_wait();
+        setTimeout(function(){},0);
         validate_token(fingerprint);
         return true;
     }
