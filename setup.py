19dd002a659691af0c0b9c274953a06c9af6ed75
Clint Savage <herlo1@gmail.com>
2015-01-03 13:33:17 -0700

    rework setup.py to use a proper application name, dnote

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+from distutils.core import setup
 
 import os
 
-DCONFIG = os.path.dirname(os.path.realpath(__file__)) + "/dconfig.py"
 DATA_DIR = os.path.dirname(os.path.realpath(__file__)) + "/data"
+DCONFIG = os.path.dirname(os.path.realpath(__file__)) + "/dnote/dconfig.py"
 
 if not os.path.exists(DCONFIG):
     with open(DCONFIG, 'w') as f:
@@ -15,3 +17,12 @@ if not os.path.exists(DCONFIG):
 
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
+
+setup(name='dnote',
+      version='1.0.1',
+      description='Self-destructing notes web app',
+      author='Aaron Toponce',
+      author_email='aaron.toponce@gmail.com',
+      url='http://github.com/atoponce/d-note/',
+      packages=['dnote'],
+     )
