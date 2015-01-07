9dec23d75ab5032914b984d310acb11c56f5810d
Clint Savage <herlo1@gmail.com>
2015-01-07 13:56:55 -0700

    update setup.py to use distribute/setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 import os
 import glob
@@ -15,21 +15,16 @@ if not os.path.exists(DCONFIG):
         f.write('duress_salt = "%s"\n' % os.urandom(16).encode('hex'))
     os.chmod(DCONFIG, 0440)
 
-data_files = [
-('share/dnote/static/css', filter(os.path.isfile, glob.glob('dnote/static/css/*.css'))),
-('share/dnote/static/img', filter(os.path.isfile, glob.glob('dnote/static/img/*'))),
-('share/dnote/static/js', filter(os.path.isfile, glob.glob('dnote/static/js/*.js'))),
-('share/dnote/templates', filter(os.path.isfile, glob.glob('dnote/templates/*'))),
-('share/dnote/data', filter(os.path.isfile, glob.glob('dnote/data/*'))),
-]
-
-setup(name='dnote',
-      version='1.0.1',
-      description='Self-destructing notes web app',
-      license='GPLv3',
-      author='Aaron Toponce',
-      author_email='aaron.toponce@gmail.com',
-      url='http://github.com/atoponce/d-note/',
-      packages=['dnote'],
-      data_files=data_files,
-     )
+setup(
+    name='dnote',
+    version='1.0.1',
+    description='Self-destructing notes web app',
+    license='GPLv3',
+    author='Aaron Toponce',
+    author_email='aaron.toponce@gmail.com',
+    url='http://github.com/atoponce/d-note/',
+    packages=['dnote'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask'],
+)
